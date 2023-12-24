2d9d76429f32e6dbbf306052c74976274035a5cf
Adam Rowland <u5799578@anu.edu.au>
2023-12-24 12:16:13 +1100

    Extended string view functionality

--- kernel/string.c
+++ kernel/string.c
@@ -9,9 +9,41 @@ uint16 cstringLength(char* cstr) {
     return result;
 }
 
-string_view svCreate(char* cstr) {
+string_view svFromCString(char* cstr) {
     string_view result;
-    result.size = cstringLength(cstr);
+    result.length = cstringLength(cstr);
     result.data = cstr;
+    return result;
+}
+
+string_view svFromSize(char* start, uint32 length) {
+    string_view result;
+    result.length = length;
+    result.data = start;
+    return result;
+}
+
+bool32 svEqualsCString(string_view sv, char* cstr) {
+    for (uint32 charIndex = 0; charIndex < sv.length; ++charIndex) {
+        if (sv.data[charIndex] != cstr[charIndex]) {
+            return false;
+        }
+    }
+    return cstr[sv.length] == '\0';
+}
+
+OPTIONAL(string_split) stringSplitOn(char* string, char delimiter) {
+    OPTIONAL(string_split) result;
+    uint32 firstLength = 0;
+    while (string[firstLength] != delimiter && string[firstLength]) {
+        firstLength++;
+    }
+    if (string[firstLength] == '\0') {
+        result.exists = false;
+    }
+    else {
+        result.inner.first = svFromSize(string, firstLength);
+        result.inner.second = svFromCString(&string[firstLength + 1]);
+    }
     return result;
 }
--- kernel/string.h
+++ kernel/string.h
@@ -1,11 +1,18 @@
 #ifndef STRING_H_
 #define STRING_H_
 
-#include "inttypes.h"
+#include "types.h"
 
 typedef struct {
     char* data;
-    uint16 size;
+    uint16 length;
 } string_view;
+
+typedef struct {
+    string_view first;
+    string_view second;
+} string_split;
+
+OPTIONAL_DEF(string_split);
 
 #endif
