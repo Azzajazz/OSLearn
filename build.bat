76d3db5ba15fa06cca87395e6977f3a9f3b0c5c1
Adam Rowland <u5799578@anu.edu.au>
2024-07-27 17:18:25 +1000

    Removed tracking of bochsrc because it is platform specific

--- build.bat
+++ build.bat
@@ -8,7 +8,7 @@ fsutil file seteof build\boot.bin 1474560
 
 echo [Build]: Building kernel binary...
 CustomTools\NASM\nasm.exe -f win32 kernel_entry.asm -o build\kernel_entry.o
-g++ -Wall -Wextra -Werror -ffreestanding -nostdlib -fno-exceptions -fno-rtti -nostartfiles -c -m32 -o build\kernel.o kernel.cpp
+g++ -Wall -Wextra -Werror -ffreestanding -nostdlib -fno-exceptions -fno-rtti -nostartfiles -c -m32 -o build\kernel.o kernel.cpp -ggdb
 ld -Map build\kernel.map -T link.ld -m i386pe -o build\kernel.tmp build\kernel_entry.o build\kernel.o
 objcopy -O binary build\kernel.tmp build\kernel.bin
 REM del kernel.tmp
